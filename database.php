a40b2fd469ea44d45e247678947aa44a42fbcdef
Susan Schutte <64018799+Sadisius@users.noreply.github.com>
2020-04-22 10:45:55 +0200

    Update database.php
    changedlogin

--- database.php
+++ database.php
@@ -1,9 +1,37 @@
-<?php
+//Create connection credentials
+$db_host = 'localhost';
+$db_name = '
+//Create connection credentials
+$db_host = 'localhost';
+$db_name = '
+';
+$db_user = 'root';
+$db_pass = '';
+
+//Create mysqli object
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+//Error Handler
+if($mysqli->connect_error){
+	printf("Connect failed: %s\n", $mysqli->connect_error);
+	exit;
+}';
+$db_user = 'root';
+$db_pass = '';
+
+//Create mysqli object
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+//Error Handler
+if($mysqli->connect_error){
+	printf("Connect failed: %s\n", $mysqli->connect_error);
+	exit;
+}<?php
 //Create connection credentials
 $db_host = 'localhost';
 $db_name = 'quizzer';
 $db_user = 'root';
-$db_pass = '';
+$db_pass = 'PinkStubble30';
 
 //Create mysqli object
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
